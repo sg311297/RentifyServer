1a488a56dd75c3e172c039816cb4790f952bd5bc
sg311297 <311297sg@gmail.com>
2024-10-27 22:00:24 +0530

    sixcommit

--- src/main/java/in/rentify/model/User.java
+++ src/main/java/in/rentify/model/User.java
@@ -14,7 +14,7 @@ import lombok.Data;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public Long id;
     
     public String firstName;
